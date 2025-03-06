d7fd8b71eee9ee1a3db9562dc70b9a4f0d09ef6d
Maurizio Manca <maurizio.manca@swisscom.com>
2025-03-06 17:34:22 +0100

    improved copy to clipboard

--- background.js
+++ background.js
@@ -83,13 +83,27 @@ chrome.action.onClicked.addListener(function (tab) {
 });
 
 function copyToClipboard(text) {
-  // Create temporary element
-  const input = document.createElement('textarea');
-  input.style.position = 'fixed';
-  input.style.opacity = '0';
-  input.value = text;
-  document.body.appendChild(input);
-  input.select();
+  //Create a textbox field where we can insert text to. 
+  var copyFrom = document.createElement("textarea");
+
+  //Set the text content to be the text you wished to copy.
+  copyFrom.textContent = text;
+
+  //Append the textbox field into the body as a child. 
+  //"execCommand()" only works when there exists selected text, and the text is inside 
+  //document.body (meaning the text is part of a valid rendered HTML element).
+  document.body.appendChild(copyFrom);
+
+  //Select all the text!
+  copyFrom.select();
+
+  //Execute command
   document.execCommand('copy');
-  document.body.removeChild(input);
+
+  //(Optional) De-select the text using blur(). 
+  copyFrom.blur();
+
+  //Remove the textbox field from the document.body, so no other JavaScript nor 
+  //other elements can get access to this.
+  document.body.removeChild(copyFrom);
 }
